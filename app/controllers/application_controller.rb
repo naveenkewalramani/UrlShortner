92d29e837fcbd50d626a2fa19d615ebdd001f8ed
Naveen Kewalramani <naveen.kewalramani2903@gmail.com>
2019-01-29 14:12:18 +0530

    filechanged

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,13 @@
 class ApplicationController < ActionController::Base
-	protect_from_forgery
-	before_action :check_session
-	
-	#This function checks whether session is set or not 
-	def check_session
-		if(session[:expires_at]!=nil)
-			if(session[:expires_at] < Time.current)
-				session[:username]=nil
-				session[:authenticate]=false
-			end
-		end
-	end
+  protect_from_forgery
+  before_action :check_session
+  #This function checks whether session is set or not 
+  def check_session 
+    if(session[:expires_at]!=nil)
+      if(session[:expires_at] < Time.current)
+        session[:username]=nil
+	session[:authenticate]=false
+      end
+    end
+  end
 end
