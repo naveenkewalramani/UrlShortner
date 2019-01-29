0102ca5556a8c80c00861b46ee7032c939b4024e
Naveen Kewalramani <naveen.kewalramani2903@gmail.com>
2019-01-29 14:13:27 +0530

    Update search_controller.rb

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,9 @@
 class SearchController < ApplicationController
   def search
- 	  if params[:url].nil?
-  	 @url = []
- 	  else
- 		 @url = Url.search(params[:url].gsub('/','//'))
- 	  end
-	end
+    if params[:url].nil?
+      @url = []
+    else
+      @url = Url.search(params[:url].gsub('/','//'))
+    end
+  end
 end
