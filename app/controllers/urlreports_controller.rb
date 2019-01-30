f7431beff9138367c7de0559c7274d874307a6f4
Naveen Kewalramani <naveen.kewalramani2903@gmail.com>
2019-01-30 11:53:03 +0530

    Update urlreports_controller.rb

--- app/controllers/urlreports_controller.rb
+++ app/controllers/urlreports_controller.rb
@@ -1,8 +1,8 @@
 class UrlreportsController < ApplicationController
 =begin
-	**Request Type:** GET
-	**Route Path:** urlreport
-	**Description:** Indexes all the records in the urlreports table and show their field on index.html.erb view page.
+  **Request Type:** GET
+  **Route Path:** urlreport
+  **Description:** Indexes all the records in the urlreports table and show their field on index.html.erb view page.
 =end
   def index
     @urlreport = Urlreport.all.order("id ASC")
